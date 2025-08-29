68f8b7778ab61193d56cd512a37f9b62efa096d0
S-P-H-E <sphe.g.personal@gmail.com>
2025-08-29 20:56:05 +0200

    clean up

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
-import {
-  Button,
-  FlatList,
-  Image,
-  Switch,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View
-} from "react-native";
+import { Button, Image, Switch, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
     <View>
-      <TextInput  placeholder="Add new todo" />
+      <TextInput placeholder="Add new todo" />
       <Button title="Add Todo" />
       <View>
         <Text>Show completed todos</Text>
@@ -25,7 +15,6 @@ export default function Index() {
             <Image source={{uri: "https://blog.acceleratelearning.com/hubfs/bigstock-education-childhood-people--123704072.jpg"}} style={{width: 200, height: 200}}/>
       </View>
 
-      {/* Component 5: TouchableOpacity */}
       <TouchableOpacity>
         <Text>Clear all todos</Text>
       </TouchableOpacity>
