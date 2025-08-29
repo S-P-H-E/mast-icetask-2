081b50f83fe74e40576687bc8755f225c4466c3e
S-P-H-E <sphe.g.personal@gmail.com>
2025-08-29 20:55:11 +0200

    ...final

---
+++ app/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  Image,
+  Switch,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <TextInput  placeholder="Add new todo" />
+      <Button title="Add Todo" />
+      <View>
+        <Text>Show completed todos</Text>
+        <Switch />
+      </View>
+
+      <View>
+            <Text>Homework</Text>
+            <Image source={{uri: "https://blog.acceleratelearning.com/hubfs/bigstock-education-childhood-people--123704072.jpg"}} style={{width: 200, height: 200}}/>
+      </View>
+
+      {/* Component 5: TouchableOpacity */}
+      <TouchableOpacity>
+        <Text>Clear all todos</Text>
+      </TouchableOpacity>
+    </View>
+  )
+} 
